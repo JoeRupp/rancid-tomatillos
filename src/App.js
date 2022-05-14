c134b2637b907a694f1448c4a64c7142e82c64cf
Nikhila Balmoori <nbalmoori@gmail.com>
2022-05-13 18:35:50 -0600

    Add movie details view

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
 import Nav from './Nav'
 import MovieContainer from './MovieContainer'
+import MovieDetails from './MovieDetails'
 import './App.css';
 import movieData from './movieData';
+import movieDetails from './movieDetailData';
+console.log(movieDetails.movie)
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      movieList: movieData.movies
+      movieList: movieData.movies,
+      currentMovie: ""
     }
+  }
+
+  chooseMovie = (id) => {
+    console.log(id)
+    this.setState({ currentMovie: movieDetails.movie })
   }
 
   render() {
     return (
       <main>
         <Nav />
-        <MovieContainer movies={this.state.movieList}/>
+        {!this.state.currentMovie && <MovieContainer movies={this.state.movieList} chooseMovie={this.chooseMovie}/>}
+        {this.state.currentMovie && <MovieDetails currentMovie={this.state.currentMovie}/>}
       </main>
     )
   }
